11c7a4675267aec51931285ff6c825dd77bc52cb
jb63 <jb63@sanger.ac.uk>
2025-03-10 11:26:13 +0000

    Remove unused entrypoints

--- src/fur_hotspot_mutations/entrypoints.py
+++
@@ -1,6 +0,0 @@
-def ping() -> str:
-    """
-    This function is used to ensure that the package is installed correctly. It
-    can be removed once the package has some real functionality.
-    """
-    return "pong"
