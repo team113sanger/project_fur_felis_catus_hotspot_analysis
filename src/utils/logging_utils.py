468c2f599d832470291039179150751ebcd02c3b
Ian Vermes <iv3@sanger.ac.uk>
2025-03-19 03:17:49 +0000

    Make logging setup function more flexible

--- src/utils/logging_utils.py
+++ src/utils/logging_utils.py
@@ -1,9 +1,13 @@
 import logging
 
 
-def setup_logging():
+def setup_logging(level: str = "DEBUG") -> None:
+    logging_level = getattr(logging, level.strip().upper())
+    if not isinstance(logging_level, int):
+        raise ValueError(f"Invalid log level: {level}")
     logging.basicConfig(
-        level=logging.DEBUG,
+        level=logging_level,
         format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
     )
+    logging.info("Logging setup complete with level: %s", level)
     return None
